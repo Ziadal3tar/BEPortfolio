f1351bd96526c0a307ec9158a3e72cbea586d11e
Ziadal3tar <ziadalattar22@gmail.com>
2023-12-28 19:02:50 +0200

    test4

--- src/module/index.routes.js
+++ src/module/index.routes.js
@@ -14,18 +14,18 @@ import ipinfo from 'ipinfo';
 export const appRouter = (app) => {
 
     app.use(express.json())
-    // app.use(expressIP().getIpInfoMiddleware);
-    // app.use(cookieParser());
-    // app.use(fingerprint());
+    app.use(expressIP().getIpInfoMiddleware);
+    app.use(cookieParser());
+    app.use(fingerprint());
 
 
 
     app.use('/project', projectRouter)
     // app.use('/EV', EV)
     app.get('/EV/CookiesId', asyncHandler(async (req, res) => {
-        let ip
+        
         if (req.ip) {
-            ip = req.ip;
+            const ip = req.ip;
             const visitorCookie = req.cookies.visitor;
             let visitor = visitorCookie ? JSON.parse(visitorCookie) : null;
             if (!visitor || typeof visitor !== 'object') {
@@ -45,6 +45,38 @@ export const appRouter = (app) => {
                     console.error('Error fetching IPinfo data:', error.message);
                 }
             }
+            
+            req.body.visitorId = visitor.ip
+            req.body.device = visitor.device
+            let visited = []
+            const allEV = await find({ model: visitorModel })
+            for (let i = 0; i < allEV.length; i++) {
+                const element = allEV[i];
+                if (element.visitorId == visitor.ip) {
+                    visited.push(element)
+                }
+            }
+            if (visited.length == 0) {
+                let AddEV = await create({ model: visitorModel, data: req.body })
+                if (AddEV) {
+                    return res.status(201).json({ message: "added successfully", AddEV })
+                } else {
+                    return res.status(401).json({ message: "added failed" })
+                }
+            } else {
+                req.body.numberOfVisits = visited[0].numberOfVisits + 1
+                let updateEV = await findOneAndUpdate({
+                    model: visitorModel,
+                    visitorId: visitor.ip,
+                    data: req.body,
+                    options: { new: true }
+                })
+                if (updateEV) {
+                    return res.status(201).json({ message: "updated", updateEV })
+                } else {
+                    return res.status(401).json({ message: "updated failed" })
+                }
+            }
         }else{
             return res.status(401).json({ message: "ip Error" })
         }
@@ -56,37 +88,7 @@ export const appRouter = (app) => {
         //----------------------
 
 
-        req.body.visitorId = visitor.ip
-        req.body.device = visitor.device
-        let visited = []
-        const allEV = await find({ model: visitorModel })
-        for (let i = 0; i < allEV.length; i++) {
-            const element = allEV[i];
-            if (element.visitorId == visitor.ip) {
-                visited.push(element)
-            }
-        }
-        if (visited.length == 0) {
-            let AddEV = await create({ model: visitorModel, data: req.body })
-            if (AddEV) {
-                return res.status(201).json({ message: "added successfully", AddEV })
-            } else {
-                return res.status(401).json({ message: "added failed" })
-            }
-        } else {
-            req.body.numberOfVisits = visited[0].numberOfVisits + 1
-            let updateEV = await findOneAndUpdate({
-                model: visitorModel,
-                visitorId: visitor.ip,
-                data: req.body,
-                options: { new: true }
-            })
-            if (updateEV) {
-                return res.status(201).json({ message: "updated", updateEV })
-            } else {
-                return res.status(401).json({ message: "updated failed" })
-            }
-        }
+
     }));
 
     app.get('/', (req, res) => res.send('portfolio apis!'))
@@ -96,6 +98,6 @@ export const appRouter = (app) => {
 
 
 
-    // app.use(globalError)
+    app.use(globalError)
     connection()
 }
