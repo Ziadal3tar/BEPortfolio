b302007e5f42199554a3248995e6a9b17d5f5e55
Ziadal3tar <ziadalattar22@gmail.com>
2023-12-28 19:03:36 +0200

    test5

--- src/module/index.routes.js
+++ src/module/index.routes.js
@@ -22,74 +22,74 @@ export const appRouter = (app) => {
 
     app.use('/project', projectRouter)
     // app.use('/EV', EV)
-    app.get('/EV/CookiesId', asyncHandler(async (req, res) => {
+    // app.get('/EV/CookiesId', asyncHandler(async (req, res) => {
         
-        if (req.ip) {
-            const ip = req.ip;
-            const visitorCookie = req.cookies.visitor;
-            let visitor = visitorCookie ? JSON.parse(visitorCookie) : null;
-            if (!visitor || typeof visitor !== 'object') {
-                visitor = {
-                    ip,
-                    country: '',
-                    city: '',
-                    device: req.headers['user-agent'],
-                    visitCount: 0,
-                };
+    //     if (req.ip) {
+    //         const ip = req.ip;
+    //         const visitorCookie = req.cookies.visitor;
+    //         let visitor = visitorCookie ? JSON.parse(visitorCookie) : null;
+    //         if (!visitor || typeof visitor !== 'object') {
+    //             visitor = {
+    //                 ip,
+    //                 country: '',
+    //                 city: '',
+    //                 device: req.headers['user-agent'],
+    //                 visitCount: 0,
+    //             };
 
-                try {
-                    const ipInfoData = await ipinfo(ip);
-                    visitor.country = ipInfoData.country;
-                    visitor.city = ipInfoData.city;
-                } catch (error) {
-                    console.error('Error fetching IPinfo data:', error.message);
-                }
-            }
-            
-            req.body.visitorId = visitor.ip
-            req.body.device = visitor.device
-            let visited = []
-            const allEV = await find({ model: visitorModel })
-            for (let i = 0; i < allEV.length; i++) {
-                const element = allEV[i];
-                if (element.visitorId == visitor.ip) {
-                    visited.push(element)
-                }
-            }
-            if (visited.length == 0) {
-                let AddEV = await create({ model: visitorModel, data: req.body })
-                if (AddEV) {
-                    return res.status(201).json({ message: "added successfully", AddEV })
-                } else {
-                    return res.status(401).json({ message: "added failed" })
-                }
-            } else {
-                req.body.numberOfVisits = visited[0].numberOfVisits + 1
-                let updateEV = await findOneAndUpdate({
-                    model: visitorModel,
-                    visitorId: visitor.ip,
-                    data: req.body,
-                    options: { new: true }
-                })
-                if (updateEV) {
-                    return res.status(201).json({ message: "updated", updateEV })
-                } else {
-                    return res.status(401).json({ message: "updated failed" })
-                }
-            }
-        }else{
-            return res.status(401).json({ message: "ip Error" })
-        }
+    //             try {
+    //                 const ipInfoData = await ipinfo(ip);
+    //                 visitor.country = ipInfoData.country;
+    //                 visitor.city = ipInfoData.city;
+    //             } catch (error) {
+    //                 console.error('Error fetching IPinfo data:', error.message);
+    //             }
+    //         }
 
-
+    //         req.body.visitorId = visitor.ip
+    //         req.body.device = visitor.device
+    //         let visited = []
+    //         const allEV = await find({ model: visitorModel })
+    //         for (let i = 0; i < allEV.length; i++) {
+    //             const element = allEV[i];
+    //             if (element.visitorId == visitor.ip) {
+    //                 visited.push(element)
+    //             }
+    //         }
+    //         if (visited.length == 0) {
+    //             let AddEV = await create({ model: visitorModel, data: req.body })
+    //             if (AddEV) {
+    //                 return res.status(201).json({ message: "added successfully", AddEV })
+    //             } else {
+    //                 return res.status(401).json({ message: "added failed" })
+    //             }
+    //         } else {
+    //             req.body.numberOfVisits = visited[0].numberOfVisits + 1
+    //             let updateEV = await findOneAndUpdate({
+    //                 model: visitorModel,
+    //                 visitorId: visitor.ip,
+    //                 data: req.body,
+    //                 options: { new: true }
+    //             })
+    //             if (updateEV) {
+    //                 return res.status(201).json({ message: "updated", updateEV })
+    //             } else {
+    //                 return res.status(401).json({ message: "updated failed" })
+    //             }
+    //         }
+    //     }else{
+    //         return res.status(401).json({ message: "ip Error" })
+    //     }
 
 
 
-        //----------------------
+
+
+    //     //----------------------
 
 
 
-    }));
+    // }));
 
     app.get('/', (req, res) => res.send('portfolio apis!'))
 
