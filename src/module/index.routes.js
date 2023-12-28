1a5f801f8a8980bcded3f0be4a376e015559566e
Ziadal3tar <ziadalattar22@gmail.com>
2023-12-28 22:28:32 +0200

    ip

--- src/module/index.routes.js
+++ src/module/index.routes.js
@@ -1,52 +1,25 @@
 import projectRouter from './project/project.routes.js'
-// import EV from './EV/EV.routes.js'
-// import express from 'express'
-// import cookieParser from 'cookie-parser';
-// import fingerprint from 'express-fingerprint';
-// import expressIP from 'express-ip';
+
 import connection from '../../DB/connection.js'
-// import { asyncHandler, globalError } from '../services/asyncHandler.js'
+import { asyncHandler, globalError } from '../services/asyncHandler.js'
 // import visitorModel from '../../DB/model/visitor.model.js'
 // import { findOneAndUpdate, find, create } from '../../DB/DBMethods.js';
-// import ipinfo from 'ipinfo';
-
-
+//
 export const appRouter = (app) => {
-
-    // app.use(express.json())
-    // app.use(expressIP().getIpInfoMiddleware);
-    // app.use(cookieParser());
-    // app.use(fingerprint());
-
 
 
     app.use('/project', projectRouter)
-    // app.use('/EV', EV)
     // app.get('/EV/CookiesId', asyncHandler(async (req, res) => {
         
-    //     if (req.ip) {
-    //         const ip = req.ip;
-    //         const visitorCookie = req.cookies.visitor;
-    //         let visitor = visitorCookie ? JSON.parse(visitorCookie) : null;
-    //         if (!visitor || typeof visitor !== 'object') {
-    //             visitor = {
-    //                 ip,
-    //                 country: '',
-    //                 city: '',
-    //                 device: req.headers['user-agent'],
-    //                 visitCount: 0,
-    //             };
 
-    //             try {
-    //                 const ipInfoData = await ipinfo(ip);
-    //                 visitor.country = ipInfoData.country;
-    //                 visitor.city = ipInfoData.city;
-    //             } catch (error) {
-    //                 console.error('Error fetching IPinfo data:', error.message);
-    //             }
-    //         }
 
-    //         req.body.visitorId = visitor.ip
+
+
+
+
+    // //     //----------------------
+
+    // req.body.visitorId = visitor.ip
     //         req.body.device = visitor.device
     //         let visited = []
     //         const allEV = await find({ model: visitorModel })
@@ -77,21 +50,10 @@ export const appRouter = (app) => {
     //                 return res.status(401).json({ message: "updated failed" })
     //             }
     //         }
-    //     }else{
-    //         return res.status(401).json({ message: "ip Error" })
-    //     }
-
-
-
-
-
-    //     //----------------------
-
-
 
     // }));
 
-    app.get('/', (req, res) => res.send('portfolio apis!'))
+    app.get('/', (req, res) => res.send(req.connection.remoteAddress))
 
 
 
