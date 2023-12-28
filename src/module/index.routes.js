630ae5b5817be5b781154bc87c7571c9f111d660
Ziadal3tar <ziadalattar22@gmail.com>
2023-12-29 00:03:31 +0200

    ip5

--- src/module/index.routes.js
+++ src/module/index.routes.js
@@ -4,22 +4,17 @@ import connection from '../../DB/connection.js'
 import { asyncHandler, globalError } from '../services/asyncHandler.js'
 // import visitorModel from '../../DB/model/visitor.model.js'
 // import { findOneAndUpdate, find, create } from '../../DB/DBMethods.js';
-// import {cookieParser} from'cookie-parser';
+import {cookieParser} from'cookie-parser';
 
 export const appRouter = (app) => {
-    // app.use(cookieParser());
+    app.use(cookieParser());
 
 
     app.use('/project', projectRouter)
-    app.get('/EV/CookiesId', asyncHandler(async (req, res) => {
+    // app.get('/EV/CookiesId', asyncHandler(async (req, res) => {
         
 
-        // const visitCount = parseInt(req.cookies.visitCount) || 0;
 
-        // // Increment the visit count and set the cookie
-        // res.cookie('visitCount', visitCount + 1, { maxAge: 365 * 24 * 60 * 60 * 1000*80, httpOnly: true });
-      
-        // res.send(req.cookies);
       
 
 
@@ -58,9 +53,16 @@ export const appRouter = (app) => {
     //             }
     //         }
 
-    }));
+    // }));
 
-    app.get('/', (req, res) => res.send('port Api'))
+    app.get('/', (req, res) => {
+                const visitCount = parseInt(req.cookies.visitCount) || 0;
+
+        // Increment the visit count and set the cookie
+        res.cookie('visitCount', visitCount + 1, { maxAge: 365 * 24 * 60 * 60 * 1000 * 80, httpOnly: true });
+      
+        res.send(req.cookies);
+    })
 
 
 
